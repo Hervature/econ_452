0943e65708c3df773989e8d9cb3d270e41335539
Zachary Hervieux-Moore <zachary.moore.1993@gmail.com>
2015-12-06 23:40:51 -0500

    Scraping all working with database saving
    Former-commit-id: 154021f65d6966ad403bb259ef43169881518b76

--- app.rb
+++ app.rb
@@ -26,43 +26,110 @@ end
 @session = Capybara::Session.new(:poltergeist)
 @session.driver.headers = { 'User-Agent' => "Safari" }
 
-@session.visit "http://www.oddsportal.com/hockey/usa/nhl-2014-2015/results/#/page/4/"
+@teams = [
+  "Anaheim Ducks",
+  "Arizona Coyotes",
+  "Boston Bruins",
+  "Buffalo Sabres",
+  "Calgary Flames",
+  "Carolina Hurricanes",
+  "Chicago Blackhawks",
+  "Colorado Avalanche",
+  "Columbus Blue Jackets",
+  "Dallas Stars",
+  "Detroit Red Wings",
+  "Edmonton Oilers",
+  "Florida Panthers",
+  "Los Angeles Kings",
+  "Minnesota Wild",
+  "Montreal Canadiens",
+  "Nashville Predators",
+  "New Jersey Devils",
+  "New York Islanders",
+  "New York Rangers",
+  "Ottawa Senators",
+  "Philadelphia Flyers",
+  "Pittsburgh Penguins",
+  "San Jose Sharks",
+  "StLouis Blues",
+  "Tampa Bay Lightning",
+  "Toronto Maple Leafs",
+  "Vancouver Canucks",
+  "Washington Capitals",
+  "Winnipeg Jets",
+]
 
-@html_doc = Nokogiri::HTML(@session.html)
+(1..29).each do |page|
+  puts page
+  # Manually change year and season and playoff and preseason
+  @session.visit "http://www.oddsportal.com/hockey/usa/nhl-2014-2015/results/#/page/#{page}/"
+  sleep(2)
+  @html_doc = Nokogiri::HTML(@session.html)
 
-@html_doc.css('tr.deactivate').each do |tr|
-  @date = tr.css('td')[0].attr('class').scan(/[t][0-9]+[-]/)[0][1..-2]
-  @date = Time.at(@date.to_i + 5*60*60).to_datetime
+  @html_doc.css('tr.deactivate').each do |tr|
+    @date = tr.css('td')[0].attr('class').scan(/[t][0-9]+[-]/)[0][1..-2]
+    @date = Time.at(@date.to_i + 5*60*60).to_datetime
 
-  @home_team, @away_team = tr.css('td')[1].text.split(" - ")
-  @home_team = @home_team.gsub(/[^A-z ]/i, '')
-  @away_team = @away_team.gsub(/[^A-z ]/i, '')
+    @home_team, @away_team = tr.css('td')[1].text.split(" - ")
+    @home_team = @home_team.gsub(/[^A-z ]/i, '')
+    @away_team = @away_team.gsub(/[^A-z ]/i, '')
 
-  @home_goals, @away_goals = tr.css('td')[2].text.split(":")
+    @home_goals, @away_goals = tr.css('td')[2].text.split(":")
 
-  # Make OT and SO ties
-  @home_goals = @home_goals.to_i
-  if @away_goals.include?('pen') || @away_goals.include?('OT') then
-    @away_goals = [@away_goals[0].to_i, @home_goals.to_i].min
-    @home_goals = @away_goals
-  else
-    @away_goals = @away_goals.to_i
+    # Make OT and SO ties
+    @home_goals = @home_goals.to_i
+    if @away_goals.include?('pen') || @away_goals.include?('OT') then
+      @away_goals = [@away_goals[0].to_i, @home_goals.to_i].min
+      @home_goals = @away_goals
+    else
+      @away_goals = @away_goals.to_i
+    end
+
+    if @home_goals > @away_goals then
+      @home_win = true
+      @away_win = false
+    elsif @home_goals < @away_goals then
+      @home_win = false
+      @away_win = true
+    else
+      @away_win = false
+      @home_win = false
+    end
+
+    @home_odds = tr.css('td')[3].css('a').text.to_f
+    @tie_odds = tr.css('td')[4].css('a').text.to_f
+    @away_odds = tr.css('td')[5].css('a').text.to_f
+
+    @playoff_game = false
+    @preseason_game = false
+
+    if @date.strftime('%Y').to_i == 2015 && (@date.strftime('%m').to_i > 4 || (@date.strftime('%m').to_i == 4 && @date.strftime('%d').to_i > 13)) then
+      @playoff_game = true
+    elsif @date.strftime('%Y').to_i == 2014 && (@date.strftime('%m').to_i < 10 || (@date.strftime('%m').to_i == 10 && @date.strftime('%d').to_i < 6)) then
+      @preseason_game = true
+    end
+
+    # Skip if All Star game
+    next if @teams.index(@home_team).nil? || @teams.index(@away_team).nil?
+
+    # Save record
+    @g = Game.new
+    @g.home_team = @home_team
+    @g.home_id = @teams.index(@home_team) + 1
+    @g.away_team = @away_team
+    @g.away_id = @teams.index(@away_team) + 1
+    @g.home_goals = @home_goals
+    @g.away_goals = @away_goals
+    @g.home_win = @home_win
+    @g.away_win = @away_win
+    @g.date = @date
+    @g.season = 2014
+    @g.home_odds = @home_odds
+    @g.tie_odds = @tie_odds
+    @g.away_odds = @away_odds
+    @g.playoff_game = @playoff_game
+    @g.preseason_game = @preseason_game
+
+    @g.save
   end
-
-  if @home_goals > @away_goals then
-    @home_win = true
-    @away_win = false
-  elsif @home_goals < @away_goals then
-    @home_win = false
-    @away_win = true
-  else
-    @away_win = false
-    @home_win = false
-  end
-
-  @home_odds = tr.css('td')[3].css('a').text.to_f
-  @tie_odds = tr.css('td')[4].css('a').text.to_f
-  @away_odds = tr.css('td')[5].css('a').text.to_f
-
-  puts @home_team + " vs. " + @away_team + " played on " + @date.strftime('%m/%d/%Y') + " score was " + @home_goals.to_s + ":" + @away_goals.to_s + " home, tie, aways odds were: " + @home_odds.to_s + ", " + @tie_odds.to_s + ", " + @away_odds.to_s + "."
 end
